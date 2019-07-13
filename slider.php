b5b7b683d74d3dd6c4315788e9102af8251a5006
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-13 12:13:35 -0700

    @get_template_part('slider')

---
+++ slider.php
@@ -0,0 +1,81 @@
+    <!--Main Slider-->
+    <section id="home-slider-v1" class="slider-v1">
+        <div class="main-slider-v1 owl-item">
+            <div class="item" style="background-image:url(<?php echo get_template_directory_uri();?>/images/resource/home-slide-1.jpg);">
+                <div class="overlay"></div>
+                <div class="slide-description">
+                    <div class="animate-item">
+                        <h2>Welcome to Our Car Care House!!!</h2>
+                    </div>
+                    <div class="animate-item">
+                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                    </div>
+                    <div class="animate-item">
+                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
+                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item" style="background-image:url(<?php echo get_template_directory_uri();?>/images/resource/home-slide-2.jpg);">
+                <div class="overlay"></div>
+                <div class="slide-description">
+                    <div class="animate-item">
+                        <h2>Welcome to Our Car Care House!!!</h2>
+                    </div>
+                    <div class="animate-item">
+                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                    </div>
+                    <div class="animate-item">
+                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
+                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item" style="background-image:url(<?php echo get_template_directory_uri();?>/images/resource/home-slide-3.jpg);">
+                <div class="overlay"></div>
+                <div class="slide-description">
+                    <div class="animate-item">
+                        <h2>Welcome to Our Car Care House!!!</h2>
+                    </div>
+                    <div class="animate-item">
+                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                    </div>
+                    <div class="animate-item">
+                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
+                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item" style="background-image:url(<?php echo get_template_directory_uri();?>/images/resource/home-slide-4.jpg);">
+                <div class="overlay"></div>
+                <div class="slide-description">
+                    <div class="animate-item">
+                        <h2>Welcome to Our Car Care House!!!</h2>
+                    </div>
+                    <div class="animate-item">
+                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                    </div>
+                    <div class="animate-item">
+                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
+                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
+                    </div>
+                </div>
+            </div>
+            <div class="item" style="background-image:url(<?php echo get_template_directory_uri();?>/images/resource/home-slide-5.jpg);">
+                <div class="overlay"></div>
+                <div class="slide-description">
+                    <div class="animate-item">
+                        <h2>Welcome to Our Car Care House!!!</h2>
+                    </div>
+                    <div class="animate-item">
+                        <h3>Top One Car Care in your city with very good quality Engr.</h3>
+                    </div>
+                    <div class="animate-item">
+                        <a class="btn-1 btn btn-primary" href="#">Contact</a>
+                        <a class="btn-2 btn btn-primary" href="#">Appoinment</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Slider End -->
