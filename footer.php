fb792b170d05d208099cb846ce7dd94bb59cf14e
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-13 12:07:39 -0700

    @get_template_part("sidebar")

--- footer.php
+++ footer.php
@@ -1,3 +1,71 @@
+<div id="subscribe">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
+                    <h1 class="subs-title">Subscribe to news letter :</h1>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12" id="subcrb-form">
+                    <form class="subscription registerForm">
+                        <div class="input-group">
+                            <input type="email" name="formInput[email]" value="" class="form-control" placeholder="Your Email:" required="" title="valid email is required" id="emaill">
+                            <span class="input-group-btn">
+                            <button class="btn btn-secondary" type="submit" value="submit" id="submit-btn"><i class="fa fa-paper-plane-o"></i></button>
+                          </span>
+                        </div>
+                        <input type="hidden" name="action" value="submitform">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!--=== Right Fixed Appiontment Form ===-->
+    <div id="appiontment-now-box">
+        <p class="toggle">APPOINTMENT</p>
+        <div class="appiontment-now ">
+            <div class="appiontment-form">
+                <p>Instant online Appiontment</p>
+                <form name="contact_us_popup" method="post" action="contact.php">
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" placeholder="Enter Name" class="form-control required name" name="name" aria-required="true" id="name1" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="email" placeholder="Email" class="form-control required email" name="email" aria-required="true" id="email" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input name="form_appontment_date" class="form-control required date-picker" type="text" placeholder="Date" aria-required="true" id="datepicker1" required>
+                            </div>
+                        </div>
+                        <div class="col-sm-6">
+                            <div class="form-group">
+                                <input type="text" placeholder="Phone" class="form-control" name="phone" id="phone" required>
+                            </div>
+                        </div>
+
+                        <div class="col-sm-12">
+                            <div class="form-group">
+                                <input type="text" placeholder="Vehicle Registration Num* " class="form-control required" name="subject" id="reg1" required>
+                            </div>
+                        </div>
+
+                    </div>
+                    <div class="form-group">
+                        <textarea placeholder="Message" rows="7" class="form-control required" name="message" required></textarea>
+                    </div>
+                    <div class="form-group tac">
+                        <button class="btn btn-primary transition7s" type="submit">APPOINTMENT Now</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+<!--===/ Right Fixed Appiontment Form ===-->
+<!-- Footer ============================================= -->
 <div id="footer-area">
         <div class="container">
             <div class="row">
---
+++ sidebar.php
@@ -0,0 +1,65 @@
+<div class="col-lg-4 col-md-4 col-sm-6 col-xs-12">
+                    <div class="sidebar-wrap">
+                        <div class="siderbar-widget">
+                            <h4 class="sidebar-widget-title">Search</h4>
+                            <div class="search-form">
+                                <input class="search-field" placeholder="Search" value="" type="text">
+                                <button type="submit"><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                        <div class="siderbar-widget">
+                            <h4 class="sidebar-widget-title">Category</h4>
+                            <ul>
+                                <li><a href="#">Higher Education<span>(15)</span></a></li>
+                                <li><a href="#">Healthcare <span>(112)</span></a></li>
+                                <li><a href="#">Senior Living <span>(4)</span></a></li>
+                                <li><a href="#">Commercial <span>(14)</span></a></li>
+                                <li><a href="#">Speciality <span>(90)</span></a></li>
+                            </ul>
+                        </div>
+                        <div class="siderbar-widget">
+                            <h4 class="sidebar-widget-title">ARCHIVES</h4>
+                            <ul>
+                                <li><a href="#">January 2017 <span>(15)</span></a></li>
+                                <li><a href="#">Februaty 2017 <span>(112)</span></a></li>
+                                <li><a href="#">March 2017 <span>(4)</span></a></li>
+                                <li><a href="#">April 2017<span>(14)</span></a></li>
+                                <li><a href="#">May 2017 <span>(90)</span></a></li>
+                            </ul>
+                        </div>
+                        <div class="siderbar-widget">
+                            <h4 class="sidebar-widget-title">RECENT NEWS</h4>
+                            <div class="widget-news">
+                                <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/resource/blog-1.jpg" alt=""></a>
+                                <div class="news-text">
+                                    <p>The Act makes provision for the interpretation of Acts of Parliament.</p>
+                                    <a class="" href="#">Read More</a>
+                                </div>
+                            </div>
+                            <div class="widget-news">
+                                <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/resource/blog-2.jpg" alt=""></a>
+                                <div class="news-text">
+                                    <p>The Act makes provision for the interpretation of Acts of Parliament.</p>
+                                    <a class="" href="#">Read More</a>
+                                </div>
+                            </div>
+                            <div class="widget-news">
+                                <a href="#"><img src="<?php echo get_template_directory_uri();?>/images/resource/blog-3.jpg" alt=""></a>
+                                <div class="news-text">
+                                    <p>The Act makes provision for the interpretation of Acts of Parliament.</p>
+                                    <a class="" href="#">Read More</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="siderbar-widget">
+                            <h4 class="sidebar-widget-title">Tags</h4>
+                            <ul class="tag-list">
+                                <li><a class="active transition7s" href="#">HTML</a></li>
+                                <li><a class="transition7s" href="#">CSS3</a></li>
+                                <li><a class="transition7s" href="#">PHP</a></li>
+                                <li><a class="transition7s" href="#">Wordpress</a></li>
+                                <li><a class="transition7s" href="#">Joomla</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
