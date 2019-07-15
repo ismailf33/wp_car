7c0823b6d4f0a2f988f2a9fd37684bea300d63d3
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-14 21:16:06 -0700

    @Custom search(searchform.php)

--- header.php
+++ header.php
@@ -57,12 +57,7 @@
                             </div>
                         </div>
                         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-3 social">
-                            <div class="header-top-search">
-                                <form action="#">
-                                    <input type="text" placeholder="search">
-                                    <button type="submit"><i class="fa fa-search"></i></button>
-                                </form>
-                            </div>
+                         <?php get_search_form();?>  
                         </div>
                     </div>
                 </div>
