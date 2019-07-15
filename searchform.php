1da33e4c1387d4c4083d0164c921317a7e1c960a
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-15 08:50:49 -0700

    @category.php page

--- searchform.php
+++ searchform.php
@@ -1,6 +1,6 @@
 <div class="header-top-search">
 <form action="<?php site_url('/');?>"  method="get" >
-    <input type="search" name="s" placeholder="<?php echo esc_attr_x('Search...', 'placeholder', 'carnews'); ?>" value="<?php echo get_search_query();?>">
+    <input type="search" name="s" placeholder="<?php echo esc_attr_x(' Search...', 'placeholder', 'carnews'); ?>" value="<?php echo get_search_query();?>">
     <button type="submit"><i class="fa fa-search"></i></button>
 </form>
 </div>
