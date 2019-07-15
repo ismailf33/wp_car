545bd672e97131662b19d08bde81a0d504079991
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-15 15:01:16 -0700

    @instant open comments box .

--- header.php
+++ header.php
@@ -32,6 +32,11 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+<?php 
+if(is_singular() && comments_open() && get_option('thread_comments')){
+wp_enqueue_scripts('comment_replay');
+}
+?>
 <?php wp_head();?>
 </head>
 
