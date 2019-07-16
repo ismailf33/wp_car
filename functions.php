5c89823338ea0f174329c3bfb7d60c5dc1d3deb4
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-15 20:29:09 -0700

    @comments field shifted top to bottom

--- functions.php
+++ functions.php
@@ -55,4 +55,11 @@ function wp_car_widgets(){
     }
 add_action('widgets_init', 'wp_car_widgets');
 
+function commnets_move_top_to_bottom($fields){
+$comment_field = $fields['name'];
+unset($fields['name']);
+$fields['name'] = $comment_field;
+return $fields;
+}
+add_action('comment_form_fields', 'commnets_move_top_to_bottom'); 
 
