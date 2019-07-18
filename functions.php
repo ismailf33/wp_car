6e786e472f18eb36dfdbc9871c7d2b7bdb487bcc
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-17 18:38:18 -0700

    .

--- functions.php
+++ functions.php
@@ -10,7 +10,6 @@ add_action('wp_enqueue_scripts' , 'wp_car_enqueue');
 function wp_car_enqueue(){
     wp_enqueue_style('master' , get_template_directory_uri().'/css/master.css');
     wp_enqueue_style('core' , get_stylesheet_uri() );
-
     wp_enqueue_script('Bootsrap' , get_template_directory_uri().'/js/bootstrap.min.js' , array('jquery') , 2.0,true);
     wp_enqueue_script( 'Wow', get_template_directory_uri().'/js/wow.min.js', array('jquery'), 2.0 , true);
     wp_enqueue_script('Carousel' , get_template_directory_uri().'/js/owlcarousel/owl.carousel.min.js' , array('jquery') , 2.0,true);
@@ -18,15 +17,13 @@ function wp_car_enqueue(){
     wp_enqueue_script('jQuery-counter' , get_template_directory_uri().'/js/waypoints.min.js' , array('jquery') , 2.0,true);
     wp_enqueue_script( 'jQuery-counter-up', get_template_directory_uri().'/js/jquery.counterup.min.js', array('jquery'), 2.0 , true);
     wp_enqueue_script('Carousel' , get_template_directory_uri().'/js/accordian-a2r.js' , array('jquery') , 2.0,true);
-    wp_enqueue_script( 'hero-menu', get_template_directory_uri().'/js/jquery-ui.min.js', array('jquery'), 2.0 , true);
-   
+    wp_enqueue_script( 'hero-menu', get_template_directory_uri().'/js/jquery-ui.min.js', array('jquery'), 2.0 , true); 
     wp_enqueue_script( 'hero-menu-up', get_template_directory_uri().'/js/jquery.meanmenu.js', array('jquery'), 2.0 , true);
     wp_enqueue_script('Skillbar' , get_template_directory_uri().'/js/skill.bars.jquery.js' , array('jquery') , 2.0,true);
     wp_enqueue_script( 'mixit-up-control', get_template_directory_uri().'/js/filterize-controls.js', array('jquery'), 2.0 , true);
     wp_enqueue_script('mixit-up' , get_template_directory_uri().' /js/jquery.filterizr.min.js' , array('jquery') , 2.0,true);
     wp_enqueue_script( 'magnific', get_template_directory_uri().'/js/jquery.magnific-popup.min.js', array('jquery'), 2.0 , true);
-    wp_enqueue_script( 'Custom', get_template_directory_uri().'/js/custom.js', array('jquery'), 2.0 , true);
-    
+    wp_enqueue_script( 'Custom', get_template_directory_uri().'/js/custom.js', array('jquery'), 2.0 , true);   
     }
 function wp_car_menu() {
     if(function_exists('register_nav_menu')){
