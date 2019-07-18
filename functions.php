b94dbf12eeddf9d0325e74474d27dca175180c6e
user.name : "alif33 <user.email : "alifhasan332@gmail.com>
2019-07-18 10:31:05 -0700

    @Slider categoris functionalities registrition

--- functions.php
+++ functions.php
@@ -134,4 +134,21 @@ function slider_gallery() {
     'menu_position' => 2, 
     'menu_icon' => 'dashicons-products', 
     'supports' => array( 'title', 'thumbnail', 'editor' )));
-        }
+ }
+// Enabling Custom Post Taxonomy (Slider gallery)
+function slider_gallery_taxonomy() { 
+    register_taxonomy(
+    'slider_cat', //The name of the taxonomy. Name should be in slug form (must not contain capital letters or spaces). 
+    'slider-items', //post type name 
+    array(
+    'hierarchical' => true, 
+    'label' => 'Slider Category', //Display name 
+    'query_var' => true, 
+    'rewrite' => array(
+    'slug' => 'slider-category', // This controls the base slug that will display before each term 
+    'with_front' => true // Don't display the category base before
+            )
+        )
+    );
+}  
+add_action( 'init', 'slider_gallery_taxonomy');
